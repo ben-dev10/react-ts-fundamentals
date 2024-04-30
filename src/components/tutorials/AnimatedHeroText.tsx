eda5ba14ec3d14025cb94135343bf283122c71a8
ben-dev10 <calvinklien025@gmail.com>
2024-04-30 22:42:11 +0000

    feat: minor edits

--- src/components/tutorials/AnimatedHeroText.tsx
+++ src/components/tutorials/AnimatedHeroText.tsx
@@ -1,4 +1,4 @@
-import { easeInOut, motion } from "framer-motion";
+import { easeOut, motion } from "framer-motion";
 
 const fadeUp = {
   y: [10, 0],
@@ -7,8 +7,9 @@ const fadeUp = {
 
 const fadeUpTransition = {
   duration: 0.55,
+  delay: 0.2,
   times: [0, 0.75, 1],
-  ease: easeInOut,
+  ease: easeOut,
 };
 
 export default function AnimatedHeroText() {
@@ -24,7 +25,7 @@ export default function AnimatedHeroText() {
         </motion.h1>
         <motion.p
           animate={fadeUp}
-          transition={{ delay: 0.25, ease: easeInOut, times: [0, 0.75, 1] }}
+          transition={{ delay: 0.3, ease: easeOut, times: [0, 0.75, 1] }}
           className="text-center mt-5 text-gray-400 text-[18px] mx-auto max-w-[450px]"
         >
           Buy and sell skateboarding gears from independent brands and stores
@@ -32,12 +33,17 @@ export default function AnimatedHeroText() {
         </motion.p>
         <motion.div
           animate={{ opacity: [0, 0.6, 1] }}
-          transition={{ delay: 0.4, ease: easeInOut, times: [0, 0.75, 1] }}
+          transition={{
+            duration: 0.5,
+            delay: 0.4,
+            ease: easeOut,
+            times: [0, 0.75, 1],
+          }}
           className="flex gap-3 justify-center mt-4"
         >
-          <button className="px-3 py-2 text-[12px] bg-white text-black rounded-md">
+          <motion.button whileTap={{scale:0.9}} className="px-3 py-2 text-[12px] bg-white text-black rounded-md">
             Buy now
-          </button>
+          </motion.button>
           <button className="px-3 py-2 text-[12px]  text-white rounded-md border border-gray-600">
             Sell now
           </button>
